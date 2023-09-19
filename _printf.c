7d7fee24c537e61d76771a41795dfdf36e9f84cc
VectorGits <davidoluremi0@gmail.com>
2023-09-19 02:59:41 -0700

    Modified code to fit betty style

--- _printf.c
+++ _printf.c
@@ -2,6 +2,52 @@
 #include <stdio.h>
 
 /**
+ * _putchar - A custom function to write a character to stdout.
+ * @c: The character to write.
+ *
+ * Return: The number of characters written (always 1).
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_char - Print a character.
+ * @args: The va_list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_char(va_list args)
+{
+	return (_putchar(va_arg(args, int)));
+}
+
+/**
+ * print_string - Print a string.
+ * @args: The va_list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int char_count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		_putchar(*str);
+		char_count++;
+		str++;
+	}
+
+	return (char_count);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: The format string.
  *
@@ -9,51 +55,38 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int char_count = 0;
+	va_list args;
+	int char_count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            char_count++;
-        }
-        else
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                char_count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str)
-                {
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        char_count++;
-                    }
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                char_count++;
-            }
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			char_count += _putchar(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char_count += print_char(args);
+			}
+			else if (*format == 's')
+			{
+				char_count += print_string(args);
+			}
+			else if (*format == '%')
+			{
+				char_count += _putchar('%');
+			}
+		}
+		format++;
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return char_count;
+	return (char_count);
 }
 
