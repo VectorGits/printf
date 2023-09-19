104742dd3e0ce8b789cd1ed514ee35feeece5eda
VectorGits <davidoluremi0@gmail.com>
2023-09-19 01:49:51 -0700

    Added project fiel

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct print - Structure for printer functions
+ * @type_arg: Identifier for the printer function
+ * @f: Pointer to a printer function
+ *
+ * Description: This structure holds pointers to various
+ * printer functions for handling different data types.
+ */
+
+typedef struct print
+{
+    char *type_arg;
+    int (*f)(va_list, char *, unsigned int *);
+} print_t;
+
+int _printf(const char *format, ...);
+
+int print_buf(char *buf, unsigned int nbuf);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int ev_print_func(const char *s, int index);
+
+#endif /* _MAIN_H_ */
+
