325a1ca38e355e97b3fa6c03bb367b46272ac326
Darndy <102405572+Darndy@users.noreply.github.com>
2023-09-19 01:36:27 -0700

    Update _printf.c

--- test-1/_printf.c
+++ test-1/_printf.c
@@ -10,45 +10,44 @@
  */
 int _printf(const char *format, ...)
 {
-	int char_count = 0; /* Count of printed characters */
-	int index = 0; /* Index for iterating through the format string */
-
+	int char_count = 0; /* Count character printed*/
+	int index = 0; /* Iterating index through string format */
 	va_list args;
 	va_start(args, format);
 
 	/* Loop through the format string */
 	while (format[index] != '\0')
 	{
-		/* Handle regular characters */
+		/* Regular character handled */
 		if (format[index] != '%')
 		{
 			char_count += _putchar(format[index]);
 			index++;
 		}
-		/* Handle format specifiers */
+		/* Format specifiers handled */
 		else if (format[index] == '%' && format[index + 1] != ' ')
 		{
 			switch (format[index + 1])
 			{
-				case 'c':
+				case 99 :
 					/* Print a character from variable arguments */
 					char_count += _putchar(va_arg(args, int));
 					break;
-				case 's':
+				case 115 :
 					char_count += print_custom_string(va_arg(args, char *));
 					break;
 				case '%':
 					/* Print a literal '%' character */
 					char_count += _putchar('%');
 					break;
-				case 'd':
-				case 'i':
+				case 100:
+				case 105:
 					char_count += print_custom_decimal(va_arg(args, int));
 					break;
-				case 'b':
+				case 98:
 					char_count += print_custom_binary(va_arg(args, int));
 					break;
-				case 'u':
+				case 117:
 					char_count += print_custom_unsigned(va_arg(args, unsigned int));
 					break;
 				default:
