1ede46fbf8560d8a11aedfc2d195f46319da7990
VectorGits <davidoluremi0@gmail.com>
2023-09-19 02:32:19 -0700

    modified function

--- _printf.c
+++ _printf.c
@@ -1,51 +1,59 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _printf - Format and print data with formatted output.
- * @format: Input string containing format specifiers.
+ * _printf - Produces output according to a format.
+ * @format: The format string.
  *
- * Return: The number of characters printed.
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+    va_list args;
+    int char_count = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            putchar(*format);
+            char_count++;
+        }
+        else
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                putchar(c);
+                char_count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str)
+                {
+                    while (*str)
+                    {
+                        putchar(*str);
+                        str++;
+                        char_count++;
+                    }
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                char_count++;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return char_count;
 }
+
