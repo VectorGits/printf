5604d91537c59d6da959fc1a93869776b3889ade
Darndy <godonudaniel@.com>
2023-09-20 00:48:22 -0700

    function file updated

--- 0-func-set.c
+++ 0-func-set.c
@@ -8,3 +8,27 @@ int print_char(va_list args)
 {
 	return _putchar(va_arg(args, int));
 }
+
+/**
+ * print_string - Print a string.
+ * @args: The va_list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int char_count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		_putchar(*str);
+		char_count++;
+		str++;
+	}
+
+	return char_count;
+}
