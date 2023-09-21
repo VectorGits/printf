2323b4673dc865260a8b9307d54dc03f371e1b84
Darndy <godonudaniel@.com>
2023-09-21 05:44:50 -0700

    custom coversion specifier

---
+++ pf-test-2/11-14_conv-spe.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * print_rev - writes the str in reverse
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int print_rev(va_list arguments, char *buf, unsigned int ibuf)
+{
+	char *str;
+	unsigned int i;
+	int j = 0;
+	char nill[] = "(llun)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		;
+	j = i - 1;
+	for (; j >= 0; j--)
+	{
+		ibuf = handl_buf(buf, str[j], ibuf);
+	}
+	return (i);
+}
+
+/**
+ * print_rot - writes the str in ROT13
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+
+int print_rot(va_list arguments, char *buf, unsigned int ibuf)
+{
+	char alf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *str;
+	unsigned int i, j, k;
+	char nill[] = "(avyy)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		for (k = j = 0; alf[j]; j++)
+		{
+			if (str[i] == alf[j])
+			{
+				k = 1;
+				ibuf = handl_buf(buf, rot[j], ibuf);
+				break;
+			}
+		}
+		if (k == 0)
+			ibuf = handl_buf(buf, str[i], ibuf);
+	}
+	return (i);
+}
